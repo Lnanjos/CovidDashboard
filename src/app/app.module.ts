e9b15e7d15c722b4562e1171a56db9b1a176c0dd
Leandro <lnanjos@outlook.com>
2020-05-05 23:21:15 -0300

    Title added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {PanelModule} from 'primeng/panel';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    PanelModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule
