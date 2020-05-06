b1303f55f4d3637cbea5496e3baa3b46c90d2855
Leandro <lnanjos@outlook.com>
2020-05-06 00:07:28 -0300

    Add cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardsComponent } from './components/cards/cards.component';
+import { CardModule } from 'primeng/card';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent
   ],
   imports: [
+    CardModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule
